b2a2ffac42114e048f44a1bb0675368b9bcd439e
Vadim <44135514+vodemn@users.noreply.github.com>
2022-10-23 23:09:41 +0300

    added basic dimensions

---
+++ lib/res/dimens.dart
@@ -0,0 +1,11 @@
+/// `valueM` represents the base value.
+/// All other values differs by 8dp.
+class Dimens {
+  static const double borderRadiusM = 16;
+  static const double borderRadiusL = 24;
+
+  static const double grid8 = 8;
+  static const double grid16 = 16;
+
+  static const double paddingM = 16;
+}
