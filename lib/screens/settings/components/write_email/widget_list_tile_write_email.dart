621c4c8fb7f648e569b357dcd9b15220849ba69d
Vadim <44135514+vodemn@users.noreply.github.com>
2023-01-23 21:47:18 +0300

    typo

--- lib/screens/settings/components/write_email/widget_list_tile_write_email.dart
+++ lib/screens/settings/components/write_email/widget_list_tile_write_email.dart
@@ -13,7 +13,7 @@ class WriteEmailListTile extends StatelessWidget {
       leading: const Icon(Icons.email),
       title: Text(S.of(context).writeEmail),
       onTap: () {
-        launchUrl(Uri.parse('mailto:${context.read<Environment>().issuesReportUrl}?subject=M3 Lightmeter'));
+        launchUrl(Uri.parse('mailto:${context.read<Environment>().contactEmail}?subject=M3 Lightmeter'));
       },
     );
   }
