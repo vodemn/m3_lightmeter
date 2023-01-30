ca5b30a026dd26e0e8f60db5624020dbb04bd1d3
Vadim <44135514+vodemn@users.noreply.github.com>
2023-01-30 11:38:49 +0300

    hide `DynamicColorListTile` if unavailable

--- lib/screens/settings/screen_settings.dart
+++ lib/screens/settings/screen_settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lightmeter/data/models/dynamic_colors_state.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/res/dimens.dart';
+import 'package:provider/provider.dart';
 
 import 'components/calibration/widget_list_tile_calibration.dart';
 import 'components/haptics/provider_list_tile_haptics.dart';
@@ -64,10 +66,11 @@ class SettingsScreen extends StatelessWidget {
                   ),
                   SettingsSection(
                     title: S.of(context).theme,
-                    children: const [
-                      ThemeTypeListTile(),
-                      PrimaryColorListTile(),
-                      DynamicColorListTile(),
+                    children: [
+                      const ThemeTypeListTile(),
+                      const PrimaryColorListTile(),
+                      if (context.read<DynamicColorState>() != DynamicColorState.unavailable)
+                        const DynamicColorListTile(),
                     ],
                   ),
                   SettingsSection(
