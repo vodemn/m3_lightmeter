0ac24e5d2c2fb8be4b9dbd3c13a77c668d5c0bba
Vadim <vadim.turko@gmail.com>
2023-04-09 13:35:58 +0300

    Temporarily removed firebase

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,12 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lightmeter/environment.dart';
 
 import 'application.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Application(Environment.prod()));
 }
