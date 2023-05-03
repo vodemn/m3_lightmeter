c1c77049fc49f857dbd71d5f1f566807de82ba75
Vadim <vadim.turko@gmail.com>
2023-05-03 12:14:45 +0200

    added crashlytics

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:lightmeter/environment.dart';
 
 import 'application.dart';
+import 'firebase.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeFirebase();
+
   runApp(const Application(Environment.prod()));
 }
