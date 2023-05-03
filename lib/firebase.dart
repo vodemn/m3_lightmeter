6232ad555a23e68db7b06cd9f34ec3e4b4114f22
Vadim <44135514+vodemn@users.noreply.github.com>
2023-05-03 12:45:10 +0200

    ML-51 Integrate Crashlytics (#52)
    * added crashlytics
    
    * added firebase_options to CD
    
    * Version bump
    
    * added firebase_options to CI
    
    * timeout

---
+++ lib/firebase.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+import 'firebase_options.dart';
+
+Future<void> initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
