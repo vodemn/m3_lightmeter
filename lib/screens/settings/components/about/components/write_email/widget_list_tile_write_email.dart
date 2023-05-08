394d761f4065112b8085bff47d8def1e88158a2f
Vadim <vadim.turko@gmail.com>
2023-05-08 16:45:24 +0200

    added error toast

--- lib/screens/settings/components/about/components/write_email/widget_list_tile_write_email.dart
+++ lib/screens/settings/components/about/components/write_email/widget_list_tile_write_email.dart
@@ -1,6 +1,8 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:lightmeter/environment.dart';
 import 'package:lightmeter/generated/l10n.dart';
+import 'package:lightmeter/res/dimens.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -13,11 +15,38 @@ class WriteEmailListTile extends StatelessWidget {
       leading: const Icon(Icons.email),
       title: Text(S.of(context).writeEmail),
       onTap: () {
-        launchUrl(
-          Uri.parse('mailto:${context.read<Environment>().contactEmail}?subject=M3 Lightmeter'),
-          mode: LaunchMode.externalApplication,
-        );
+        final email = context.read<Environment>().contactEmail;
+        final mailToUrl = Uri.parse('mailto:$email?subject=M3 Lightmeter');
+        canLaunchUrl(mailToUrl).then((canLaunch) {
+          if (canLaunch) {
+            launchUrl(
+              mailToUrl,
+              mode: LaunchMode.externalApplication,
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(S.of(context).youDontHaveMailApp),
+                behavior: SnackBarBehavior.floating,
+                action: SnackBarAction(
+                  label: S.of(context).copyEmail,
+                  onPressed: () {
+                    FlutterClipboard.copy(email).then((_) {
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                    });
+                  },
+                ),
+              ),
+            );
+          }
+        });
       },
     );
+  }
+
+  void _copyEmail(BuildContext context) {
+    FlutterClipboard.copy(context.read<Environment>().contactEmail).then((_) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+    });
   }
 }
