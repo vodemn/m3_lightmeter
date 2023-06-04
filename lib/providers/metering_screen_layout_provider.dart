67f8e6e2360be8cc53fabbc10b95f2b60ad44cf9
Vadim <vadim.turko@gmail.com>
2023-06-04 12:50:07 +0200

    fixed redundant vibrations

--- lib/providers/metering_screen_layout_provider.dart
+++ lib/providers/metering_screen_layout_provider.dart
@@ -3,35 +3,6 @@ import 'package:lightmeter/data/models/metering_screen_layout_config.dart';
 import 'package:lightmeter/data/shared_prefs_service.dart';
 import 'package:lightmeter/utils/inherited_generics.dart';
 
-class MeteringScreenLayout extends InheritedModelBase<MeteringScreenLayoutFeature, bool> {
-  const MeteringScreenLayout({
-    required super.data,
-    required super.child,
-    super.key,
-  });
-
-  static MeteringScreenLayoutConfig of(BuildContext context, {bool listen = true}) {
-    if (listen) {
-      return context
-          .dependOnInheritedWidgetOfExactType<
-              InheritedModelBase<MeteringScreenLayoutFeature, bool>>()!
-          .data;
-    } else {
-      return context
-          .findAncestorWidgetOfExactType<InheritedModelBase<MeteringScreenLayoutFeature, bool>>()!
-          .data;
-    }
-  }
-
-  static bool featureOf(BuildContext context, MeteringScreenLayoutFeature aspect) {
-    return InheritedModel.inheritFrom<InheritedModelBase<MeteringScreenLayoutFeature, bool>>(
-      context,
-      aspect: aspect,
-    )!
-        .data[aspect]!;
-  }
-}
-
 class MeteringScreenLayoutProvider extends StatefulWidget {
   final Widget child;
 
@@ -68,5 +39,22 @@ class MeteringScreenLayoutProviderState extends State<MeteringScreenLayoutProvid
       });
     });
     context.get<UserPreferencesService>().meteringScreenLayout = _config;
+  }
+}
+
+typedef _MeteringScreenLayoutModel = InheritedModelBase<MeteringScreenLayoutFeature, bool>;
+
+extension MeteringScreenLayout on InheritedModelBase<MeteringScreenLayoutFeature, bool> {
+  static MeteringScreenLayoutConfig of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return context.dependOnInheritedWidgetOfExactType<_MeteringScreenLayoutModel>()!.data;
+    } else {
+      return context.findAncestorWidgetOfExactType<_MeteringScreenLayoutModel>()!.data;
+    }
+  }
+
+  static bool featureOf(BuildContext context, MeteringScreenLayoutFeature aspect) {
+    return InheritedModel.inheritFrom<_MeteringScreenLayoutModel>(context, aspect: aspect)!
+        .data[aspect]!;
   }
 }
