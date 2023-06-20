d09b533a2d8d8d03f646f14996a3628d182a4d22
Vadim <vadim.turko@gmail.com>
2023-06-20 08:26:25 +0200

    `CameraContainerBloc` equality tests

--- lib/screens/metering/components/camera_container/state_container_camera.dart
+++ lib/screens/metering/components/camera_container/state_container_camera.dart
@@ -62,4 +62,14 @@ class CameraErrorState extends CameraContainerState {
   final CameraErrorType error;
 
   const CameraErrorState(this.error);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other.runtimeType != runtimeType) return false;
+    return other is CameraErrorState && other.error == error;
+  }
+
+  @override
+  int get hashCode => Object.hash(error, runtimeType);
 }
