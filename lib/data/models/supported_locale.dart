d935d7f6deb2c1b36a502686dec0fb7282dc6104
Vadim <vadim.turko@gmail.com>
2023-06-21 08:58:55 +0200

    `SupportedLocale` tests

--- lib/data/models/supported_locale.dart
+++ lib/data/models/supported_locale.dart
@@ -1,19 +1,4 @@
-import 'package:intl/intl.dart';
-
 enum SupportedLocale { en, fr, ru }
-
-SupportedLocale get currentLanguage {
-  switch (Intl.getCurrentLocale()) {
-    case "en":
-      return SupportedLocale.en;
-    case "fr":
-      return SupportedLocale.fr;
-    case "ru":
-      return SupportedLocale.ru;
-    default:
-      return SupportedLocale.en;
-  }
-}
 
 extension SupportedLocaleExtension on SupportedLocale {
   String get intlName => toString().replaceAll("SupportedLocale.", "");
