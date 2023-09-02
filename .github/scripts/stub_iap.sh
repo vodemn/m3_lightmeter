3b8a79e25efaaec614fde14ae45c3b3c69e43838
Vadim <vadim.turko@gmail.com>
2023-09-02 11:50:11 +0200

    added comment to stub_iap.sh

--- .github/scripts/stub_iap.sh
+++ .github/scripts/stub_iap.sh
@@ -1,1 +1,2 @@
+# https://unix.stackexchange.com/questions/435708/regex-multiline-pattern-and-substitution-replacement
 perl -0777 -i -pe 's/(  m3_lightmeter_iap:\n)(    git:\n      url: "https:\/\/github.com\/vodemn\/m3_lightmeter_iap"\n      ref: main)/$1    path: iap/sg' pubspec.yaml
