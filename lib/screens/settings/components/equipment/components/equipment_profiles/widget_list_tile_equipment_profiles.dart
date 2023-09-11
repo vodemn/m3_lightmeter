483453f69a9b04a7f763bd7b2792d0762857caa6
Vadim <44135514+vodemn@users.noreply.github.com>
2023-09-11 23:02:37 +0200

    `IAPBuilder` -> `IAPListTile`

--- lib/screens/settings/components/equipment/components/equipment_profiles/widget_list_tile_equipment_profiles.dart
+++ lib/screens/settings/components/equipment/components/equipment_profiles/widget_list_tile_equipment_profiles.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/screens/settings/components/equipment/components/equipment_profiles/components/equipment_profile_screen/screen_equipment_profile.dart';
-import 'package:lightmeter/screens/settings/components/shared/iap_builder/widget_builder_iap.dart';
-import 'package:m3_lightmeter_iap/m3_lightmeter_iap.dart';
+import 'package:lightmeter/screens/settings/components/shared/iap_list_tile/widget_list_tile_iap.dart';
 import 'package:m3_lightmeter_resources/m3_lightmeter_resources.dart';
 
 class EquipmentProfilesListTile extends StatelessWidget {
@@ -10,18 +9,14 @@ class EquipmentProfilesListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IAPBuilder(
-      builder: (context, status) => ListTile(
-        leading: const Icon(Icons.camera),
-        title: Text(S.of(context).equipmentProfiles),
-        onTap: status == IAPProductStatus.purchased
-            ? () {
-                Navigator.of(context).push<EquipmentProfile>(
-                  MaterialPageRoute(builder: (_) => const EquipmentProfilesScreen()),
-                );
-              }
-            : null,
-      ),
+    return IAPListTile(
+      leading: const Icon(Icons.camera),
+      title: Text(S.of(context).equipmentProfiles),
+      onTap: () {
+        Navigator.of(context).push<EquipmentProfile>(
+          MaterialPageRoute(builder: (_) => const EquipmentProfilesScreen()),
+        );
+      },
     );
   }
 }
--- lib/screens/settings/components/equipment/components/films/widget_list_tile_films.dart
+++ lib/screens/settings/components/equipment/components/films/widget_list_tile_films.dart
@@ -2,36 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:lightmeter/data/models/film.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/screens/settings/components/shared/dialog_filter/widget_dialog_filter.dart';
-import 'package:lightmeter/screens/settings/components/shared/iap_builder/widget_builder_iap.dart';
-import 'package:m3_lightmeter_iap/m3_lightmeter_iap.dart';
+import 'package:lightmeter/screens/settings/components/shared/iap_list_tile/widget_list_tile_iap.dart';
 
 class FilmsListTile extends StatelessWidget {
   const FilmsListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return IAPBuilder(
-      builder: (context, status) => ListTile(
-        leading: const Icon(Icons.camera_roll),
-        title: Text(S.of(context).filmsInUse),
-        onTap: status == IAPProductStatus.purchased
-            ? () {
-                showDialog<List<Film>>(
-                  context: context,
-                  builder: (_) => DialogFilter<Film>(
-                    icon: const Icon(Icons.camera_roll),
-                    title: S.of(context).filmsInUse,
-                    description: S.of(context).filmsInUseDescription,
-                    values: Film.values.sublist(1),
-                    selectedValues: Film.values.sublist(1),
-                    titleAdapter: (_, value) => value.name,
-                  ),
-                ).then((values) {
-                  if (values != null) {}
-                });
-              }
-            : null,
-      ),
+    return IAPListTile(
+      leading: const Icon(Icons.camera_roll),
+      title: Text(S.of(context).filmsInUse),
+      onTap: () {
+        showDialog<List<Film>>(
+          context: context,
+          builder: (_) => DialogFilter<Film>(
+            icon: const Icon(Icons.camera_roll),
+            title: S.of(context).filmsInUse,
+            description: S.of(context).filmsInUseDescription,
+            values: Film.values.sublist(1),
+            selectedValues: Film.values.sublist(1),
+            titleAdapter: (_, value) => value.name,
+          ),
+        ).then((values) {
+          if (values != null) {}
+        });
+      },
     );
   }
 }
