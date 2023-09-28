a09173441daef5dc64f73b608070c6a58bce17f5
Vadim <44135514+vodemn@users.noreply.github.com>
2023-09-28 16:15:39 +0200

    Update generate_screenshots.dart

--- integration_test/generate_screenshots.dart
+++ integration_test/generate_screenshots.dart
@@ -223,6 +223,7 @@ extension on WidgetTester {
   Future<void> takePhoto() async {
     await tap(find.byType(MeteringMeasureButton));
     await pump(const Duration(seconds: 2)); // wait for circular progress indicator
+    await pump(const Duration(seconds: 1)); // wait for circular progress indicator
     await pumpAndSettle();
   }
 
