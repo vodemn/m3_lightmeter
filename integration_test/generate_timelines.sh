161b90c6621fd803289aca6b448b77acc1c35117
Vadim <44135514+vodemn@users.noreply.github.com>
2023-10-02 11:57:59 +0200

    cleanup

--- integration_test/generate_timelines.sh
+++ integration_test/generate_timelines.sh
@@ -1,10 +1,24 @@
-flutter drive \
+flutter --version
+fvm flutter build apk \
     --dart-define="cameraPreviewAspectRatio=240/320" \
     --dart-define="cameraStubImage=assets/camera_stub_image.jpg" \
-    --driver=test_driver/performance_driver.dart \
-    --target=integration_test/metering_test.dart \
+    --target=integration_test/generate_screenshots.dart \
     --profile \
-    --flavor=dev \
-    --no-dds \
-    --endless-trace-buffer \
-    --purge-persistent-cache
+    --flavor=dev
+
+for n in {1..5}; do
+    echo "============ Run number ${n} ============"
+
+    flutter drive \
+        --dart-define="cameraPreviewAspectRatio=240/320" \
+        --dart-define="cameraStubImage=assets/camera_stub_image.jpg" \
+        --driver=test_driver/screenshot_driver.dart \
+        --target=integration_test/generate_screenshots.dart \
+        --profile \
+        --flavor=dev \
+        --no-dds \
+        --endless-trace-buffer \
+        --purge-persistent-cache \
+        --use-application-binary=build/app/outputs/flutter-apk/app-dev-profile.apk
+done
+
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:lightmeter/application.dart';
 import 'package:lightmeter/application_wrapper.dart';
 import 'package:lightmeter/environment.dart';
