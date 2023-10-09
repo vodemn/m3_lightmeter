68ccc5f01eb9c5131f79ea7cbacf71af713eb615
Vadim <44135514+vodemn@users.noreply.github.com>
2023-10-09 17:04:24 +0200

    Synced _iap_ stub with repo

---
+++ iap/lib/src/data/iap_storage_service.dart
@@ -0,0 +1,17 @@
+import 'package:m3_lightmeter_resources/m3_lightmeter_resources.dart';
+
+class IAPStorageService {
+  const IAPStorageService(Object _);
+
+  String get selectedEquipmentProfileId => '';
+  set selectedEquipmentProfileId(String id) {}
+
+  List<EquipmentProfile> get equipmentProfiles => [];
+  set equipmentProfiles(List<EquipmentProfile> profiles) {}
+
+  Film get selectedFilm => const Film.other();
+  set selectedFilm(Film value) {}
+
+  List<Film> get filmsInUse => [];
+  set filmsInUse(List<Film> profiles) {}
+}
