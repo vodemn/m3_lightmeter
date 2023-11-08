17cd99e8a9620e71b0e8902e8cdd0cf9c4a0b772
Vadim <44135514+vodemn@users.noreply.github.com>
2023-11-08 15:05:35 +0100

    hide camera features for purchasable status

--- lib/screens/settings/components/metering/components/camera_features/widget_list_tile_camera_features.dart
+++ lib/screens/settings/components/metering/components/camera_features/widget_list_tile_camera_features.dart
@@ -3,13 +3,14 @@ import 'package:lightmeter/data/models/camera_feature.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/providers/user_preferences_provider.dart';
 import 'package:lightmeter/screens/settings/components/shared/dialog_switch/widget_dialog_switch.dart';
+import 'package:lightmeter/screens/settings/components/shared/iap_list_tile/widget_list_tile_iap.dart';
 
 class CameraFeaturesListTile extends StatelessWidget {
   const CameraFeaturesListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
+    return IAPListTile(
       leading: const Icon(Icons.camera_alt),
       title: Text(S.of(context).cameraFeatures),
       onTap: () {
