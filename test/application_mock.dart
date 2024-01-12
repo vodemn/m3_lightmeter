8cf5d38c37adc4d9b715182e180519b50d84928f
Vadim <44135514+vodemn@users.noreply.github.com>
2024-01-12 16:03:04 +0100

    fixed tests

--- test/application_mock.dart
+++ test/application_mock.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/res/theme.dart';
+import 'package:m3_lightmeter_iap/m3_lightmeter_iap.dart';
 
 /// Provides [MaterialApp] with default theme and "en" localization
 class WidgetTestApplicationMock extends StatelessWidget {
+  final IAPProductStatus productStatus;
   final Widget child;
 
-  const WidgetTestApplicationMock({required this.child, super.key});
+  const WidgetTestApplicationMock({
+    this.productStatus = IAPProductStatus.purchased,
+    required this.child,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
