85c409fbe8102d15921b7204b74b17e49cfde53b
Vadim <44135514+vodemn@users.noreply.github.com>
2024-01-15 23:13:26 +0100

    ML-134 Firebase Remote Config issues (#150)
    * added try-catch to config fetch

--- lib/data/remote_config_service.dart
+++ lib/data/remote_config_service.dart
@@ -52,9 +52,15 @@ class RemoteConfigService implements IRemoteConfigService {
 
   @override
   Future<void> fetchConfig() async {
-    // https://github.com/firebase/flutterfire/issues/6196#issuecomment-927751667
-    await Future.delayed(const Duration(seconds: 1));
-    await FirebaseRemoteConfig.instance.fetch();
+    try {
+      // https://github.com/firebase/flutterfire/issues/6196#issuecomment-927751667
+      await Future.delayed(const Duration(seconds: 1));
+      await FirebaseRemoteConfig.instance.fetch();
+    } on FirebaseException catch (e) {
+      _logError('Firebase exception during Firebase Remote Config fetch: $e');
+    } catch (e) {
+      _logError('Error during Firebase Remote Config fetch: $e');
+    }
   }
 
   @override
