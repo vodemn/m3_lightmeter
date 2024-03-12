ed45052317e041c7ce2616da00874f1ca133a2d4
Vadim <44135514+vodemn@users.noreply.github.com>
2024-03-12 20:04:56 +0100

    use flutter drive for integration tests

---
+++ integration_test/run_integration_tests.sh
@@ -0,0 +1,5 @@
+flutter drive \
+    --target=integration_test/e2e_test.dart \
+    --driver=test_driver/integration_driver.dart \
+    --flavor=dev \
+    --dart-define cameraStubImage=assets/camera_stub_image.jpg
--- test_driver/integration_driver.dart
+++ test_driver/integration_driver.dart
@@ -1,8 +1,5 @@
 import 'package:integration_test/integration_test_driver_extended.dart';
 
-import 'utils/grant_camera_permission.dart';
-
 Future<void> main() async {
-  await grantCameraPermission();
   await integrationDriver();
 }
