1f30c74d18024e52a7beceb37841668c6d75295a
Vadim <44135514+vodemn@users.noreply.github.com>
2024-03-13 12:50:04 +0100

    Delete run_integration_tests.sh

--- .github/scripts/run_integration_tests.sh
+++
@@ -1,6 +0,0 @@
-flutter drive \
-    --target=integration_test/e2e_test.dart \
-    --driver=test_driver/integration_driver.dart \
-    --flavor=dev \
-    --no-dds \
-    --dart-define cameraStubImage=assets/camera_stub_image.jpg
--- integration_test/run_integration_tests.sh
+++
@@ -1,5 +0,0 @@
-flutter drive \
-    --target=integration_test/e2e_test.dart \
-    --driver=test_driver/integration_driver.dart \
-    --flavor=dev \
-    --dart-define cameraStubImage=assets/camera_stub_image.jpg
