24328c5bb8d57399b018017988bc5c586e39ed2c
Vadim <44135514+vodemn@users.noreply.github.com>
2024-04-06 19:08:13 +0200

    removed unused import

--- lib/screens/metering/components/camera_container/bloc_container_camera.dart
+++ lib/screens/metering/components/camera_container/bloc_container_camera.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'dart:math' as math;
 
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
