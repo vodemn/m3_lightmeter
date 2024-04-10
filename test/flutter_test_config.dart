56942255e25decd9c25e31b45c836ddcb5bbb7e0
Vadim <44135514+vodemn@users.noreply.github.com>
2024-04-10 23:34:22 +0200

    setup golden toolkit

---
+++ test/flutter_test_config.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await loadAppFonts();
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(defaultDevices: _defaultDevices),
+  );
+}
+
+const _defaultDevices = <Device>[
+  Device(
+    name: 'iPhone 8',
+    size: Size(375, 667),
+    devicePixelRatio: 2.0,
+  ),
+  Device(
+    name: 'iPhone 13 Pro',
+    size: Size(390, 844),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  ),
+  Device(
+    name: 'iPhone 15 Pro Max',
+    size: Size(430, 932),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  ),
+];
