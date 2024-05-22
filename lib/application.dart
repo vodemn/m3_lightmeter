d4deae57efb3503e606a8f3d5835efb11ce4f1fd
Vadim <44135514+vodemn@users.noreply.github.com>
2024-05-22 22:50:47 +0200

    Fixed release notes dialog logic

--- lib/application.dart
+++ lib/application.dart
@@ -7,6 +7,7 @@ import 'package:lightmeter/platform_config.dart';
 import 'package:lightmeter/providers/user_preferences_provider.dart';
 import 'package:lightmeter/screens/metering/flow_metering.dart';
 import 'package:lightmeter/screens/settings/flow_settings.dart';
+import 'package:lightmeter/screens/shared/release_notes_dialog/flow_dialog_release_notes.dart';
 import 'package:lightmeter/screens/timer/flow_timer.dart';
 
 class Application extends StatelessWidget {
@@ -41,7 +42,7 @@ class Application extends StatelessWidget {
         ),
         initialRoute: "metering",
         routes: {
-          "metering": (_) => const MeteringFlow(),
+          "metering": (_) => const ReleaseNotesFlow(child: MeteringFlow()),
           "settings": (_) => const SettingsFlow(),
           "timer": (context) => TimerFlow(args: ModalRoute.of(context)!.settings.arguments! as TimerFlowArgs),
         },
