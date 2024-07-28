9d5860a53f9760ab31850abe259f5c05bd9caff6
Vadim <44135514+vodemn@users.noreply.github.com>
2024-07-28 14:37:37 +0200

    ML-184 Make "Restore purchases" option more accessible (#185)
    * moved "Restore purchases" to the Pro section
    
    * updated goldens
    
    * downgraded dependencies
    
    * upgraded again
    
    * added resources to dependency_overrides

--- lib/screens/settings/components/about/widget_settings_section_about.dart
+++ lib/screens/settings/components/about/widget_settings_section_about.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/screens/settings/components/about/components/report_issue/widget_list_tile_report_issue.dart';
-import 'package:lightmeter/screens/settings/components/about/components/restore_purchases/widget_list_tile_restore_purchases.dart';
 import 'package:lightmeter/screens/settings/components/about/components/source_code/widget_list_tile_source_code.dart';
 import 'package:lightmeter/screens/settings/components/about/components/version/widget_list_tile_version.dart';
 import 'package:lightmeter/screens/settings/components/about/components/write_email/widget_list_tile_write_email.dart';
 import 'package:lightmeter/screens/settings/components/shared/settings_section/widget_settings_section.dart';
-import 'package:lightmeter/utils/context_utils.dart';
 
 class AboutSettingsSection extends StatelessWidget {
   const AboutSettingsSection({super.key});
@@ -15,12 +13,11 @@ class AboutSettingsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return SettingsSection(
       title: S.of(context).about,
-      children: [
-        const SourceCodeListTile(),
-        if (!context.isPro) const RestorePurchasesListTile(),
-        const ReportIssueListTile(),
-        const WriteEmailListTile(),
-        const VersionListTile(),
+      children: const [
+        SourceCodeListTile(),
+        ReportIssueListTile(),
+        WriteEmailListTile(),
+        VersionListTile(),
       ],
     );
   }
