c166f5d4d5fd11ab6aa1e0c0e91fbf6294f303a1
Vadim <44135514+vodemn@users.noreply.github.com>
2024-07-28 14:05:50 +0200

    moved "Restore purchases" to the Pro section

--- lib/screens/settings/components/lightmeter_pro/widget_settings_section_lightmeter_pro.dart
+++ lib/screens/settings/components/lightmeter_pro/widget_settings_section_lightmeter_pro.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/screens/settings/components/lightmeter_pro/components/buy_pro/widget_list_tile_buy_pro.dart';
+import 'package:lightmeter/screens/settings/components/lightmeter_pro/components/restore_purchases/widget_list_tile_restore_purchases.dart';
 import 'package:lightmeter/screens/settings/components/shared/settings_section/widget_settings_section.dart';
 
 class LightmeterProSettingsSection extends StatelessWidget {
@@ -12,7 +13,10 @@ class LightmeterProSettingsSection extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.secondary,
       foregroundColor: Theme.of(context).colorScheme.onSecondary,
       title: S.of(context).proFeaturesTitle,
-      children: const [BuyProListTile()],
+      children: const [
+        BuyProListTile(),
+        RestorePurchasesListTile(),
+      ],
     );
   }
 }
