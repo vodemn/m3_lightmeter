2aa8e66d0f212de27e5a50cbc0e9f6bd34a5f6ca
Vadim <44135514+vodemn@users.noreply.github.com>
2025-01-27 12:02:53 +0100

    downgraded to Flutter 3.24.5

--- lib/utils/color_to_int.dart
+++ lib/utils/color_to_int.dart
@@ -2,11 +2,14 @@ import 'dart:ui';
 
 extension ColorToInt on Color {
   int toInt() {
-    final a = (this.a * 255).round();
-    final r = (this.r * 255).round();
-    final g = (this.g * 255).round();
-    final b = (this.b * 255).round();
+    return value;
 
-    return (a << 24) | (r << 16) | (g << 8) | b;
+    // Wait for Flutter 3.27.1
+    // final a = (this.a * 255).round();
+    // final r = (this.r * 255).round();
+    // final g = (this.g * 255).round();
+    // final b = (this.b * 255).round();
+
+    // return (a << 24) | (r << 16) | (g << 8) | b;
   }
 }
