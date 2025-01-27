74a6091f19f2a80ada6033204e8ca2a16cf8e0b3
Vadim <44135514+vodemn@users.noreply.github.com>
2025-01-27 21:08:16 +0100

    Update update_goldens.sh

--- update_goldens.sh
+++ update_goldens.sh
@@ -1,6 +1,6 @@
 defaults -currentHost write -g AppleFontSmoothing -int 0
 goldens=$(find ./test -name "*_golden_test.dart" -print)
 for f in $goldens; do
-    fvm flutter test "$f" --dart-define cameraStubImage=assets/camera_stub_image.jpg --update-goldens
+    flutter test "$f" --dart-define cameraStubImage=assets/camera_stub_image.jpg --update-goldens
 done
 defaults -currentHost write -g AppleFontSmoothing -int 3
