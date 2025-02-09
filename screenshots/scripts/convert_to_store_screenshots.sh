4e42c395586a72e26c8f34d1247c9e36371e1cf1
Vadim <44135514+vodemn@users.noreply.github.com>
2025-02-09 22:51:27 +0100

    updated ios screenshots

--- screenshots/scripts/convert_to_store_screenshots.sh
+++ screenshots/scripts/convert_to_store_screenshots.sh
@@ -1,4 +1,4 @@
-dart run screenshots/convert_to_store_screenshots.dart -p android -d pixel_6 -l android
-dart run screenshots/convert_to_store_screenshots.dart -p ios -d iphone_13_pro -l iphone55inch
-dart run screenshots/convert_to_store_screenshots.dart -p ios -d iphone_13_pro -l iphone65inch
-#dart run screenshots/convert_to_store_screenshots.dart -p ios -d ipad_pro_12.9-inch_6th_generation -l ipad13inch
+#dart run screenshots/convert_to_store_screenshots.dart -p android -d pixel_6 -l android
+#dart run screenshots/convert_to_store_screenshots.dart -p ios -d iphone_13_pro -l iphone55inch
+#dart run screenshots/convert_to_store_screenshots.dart -p ios -d iphone_13_pro -l iphone65inch
+dart run screenshots/convert_to_store_screenshots.dart -p ios -d ipad_pro_12.9-inch_6th_generation -l ipad13inch
