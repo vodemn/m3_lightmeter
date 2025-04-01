79f702f7eaf78d5d24e4748dcf367726255eb61a
Vadim <44135514+vodemn@users.noreply.github.com>
2025-04-01 22:35:24 +0200

    ML-228 Name textfield keeps focus after editing on other sections (#229)
    * unfocus textfield when tapped outside
    
    * autofocus equipment profile name
    
    * upload diagnostic screenshots
    
    * typo
    
    * wip
    
    * typo
    
    * more diagnostic screenshots
    
    * removed diagnostic screenshots
    
    * returned diagnostic screenshots
    
    * skip failing step (wip)
    
    * cleanup

--- lib/screens/shared/text_field/widget_text_field.dart
+++ lib/screens/shared/text_field/widget_text_field.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class LightmeterTextField extends TextFormField {
-  LightmeterTextField({
-    super.controller,
-    super.autofocus,
-    super.initialValue,
-    super.inputFormatters,
-    super.maxLength,
-    super.onChanged,
-    super.style,
-    super.textAlign,
-    Widget? leading,
-    String? hintText,
-  }) : super(
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          maxLines: 1,
-          decoration: InputDecoration(
-            counter: const SizedBox(),
-            contentPadding: EdgeInsets.zero,
-            errorStyle: const TextStyle(fontSize: 0),
-            icon: leading,
-            hintText: hintText,
-          ),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return '';
-            } else {
-              return null;
-            }
-          },
-        );
+class LightmeterTextField extends StatefulWidget {
+  const LightmeterTextField({
+    this.autofocus = false,
+    this.controller,
+    this.hintText,
+    this.initialValue,
+    this.inputFormatters,
+    this.leading,
+    this.maxLength,
+    this.onChanged,
+    this.style,
+    this.textAlign = TextAlign.start,
+  });
+
+  final bool autofocus;
+  final TextEditingController? controller;
+  final String? hintText;
+  final String? initialValue;
+  final List<TextInputFormatter>? inputFormatters;
+  final Widget? leading;
+  final int? maxLength;
+  final void Function(String)? onChanged;
+  final TextStyle? style;
+  final TextAlign textAlign;
+
+  @override
+  State<LightmeterTextField> createState() => _LightmeterTextFieldState();
+}
+
+class _LightmeterTextFieldState extends State<LightmeterTextField> {
+  late final focusNode = FocusNode(debugLabel: widget.hintText);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autofocus: widget.autofocus,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      controller: widget.controller,
+      focusNode: focusNode,
+      initialValue: widget.initialValue,
+      inputFormatters: widget.inputFormatters,
+      maxLength: widget.maxLength,
+      onChanged: widget.onChanged,
+      style: widget.style,
+      textAlign: widget.textAlign,
+      decoration: InputDecoration(
+        counter: const SizedBox(),
+        contentPadding: EdgeInsets.zero,
+        errorStyle: const TextStyle(fontSize: 0),
+        icon: widget.leading,
+        hintText: widget.hintText,
+      ),
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '';
+        } else {
+          return null;
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
 }
