287864911bf2fc582c131c3f43d18c3d26b7568c
Vadim <44135514+vodemn@users.noreply.github.com>
2025-07-21 22:03:27 +0200

    made logbook iap

--- lib/screens/settings/components/camera/logbook/widget_list_tile_logbook.dart
+++ lib/screens/settings/components/camera/logbook/widget_list_tile_logbook.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:lightmeter/generated/l10n.dart';
 import 'package:lightmeter/screens/logbook/screen_logbook.dart';
+import 'package:lightmeter/screens/settings/components/shared/iap_list_tile/widget_list_tile_iap.dart';
 
 class LogbookListTile extends StatelessWidget {
   const LogbookListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
+    return IAPListTile(
       leading: const Icon(Icons.book_outlined),
       title: Text(S.of(context).logbook),
       onTap: () {
