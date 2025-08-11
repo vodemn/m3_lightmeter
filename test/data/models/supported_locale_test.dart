056cf4f44bf215bafa85ae95ccab7fd866f9d445
Vadim <44135514+vodemn@users.noreply.github.com>
2025-08-11 11:09:29 +0200

    ML-247 Revise translations + Add PL locale (#249)
    * wip
    
    * wip
    
    * reviewed punctuation
    
    * Create intl_pl.arb
    
    * Update metering_screen.png
    
    * fixed polish translations
    
    * shortened Russian translations
    
    * Added missing translations
    
    * Update intl_ru.arb

--- test/data/models/supported_locale_test.dart
+++ test/data/models/supported_locale_test.dart
@@ -5,6 +5,7 @@ void main() {
   test('intlName', () {
     expect(SupportedLocale.en.intlName, 'en');
     expect(SupportedLocale.fr.intlName, 'fr');
+    expect(SupportedLocale.pl.intlName, 'pl');
     expect(SupportedLocale.ru.intlName, 'ru');
     expect(SupportedLocale.zh.intlName, 'zh');
     expect(SupportedLocale.de.intlName, 'de');
@@ -13,6 +14,7 @@ void main() {
   test('localizedName', () {
     expect(SupportedLocale.en.localizedName, 'English');
     expect(SupportedLocale.fr.localizedName, 'Français');
+    expect(SupportedLocale.pl.localizedName, 'Polski');
     expect(SupportedLocale.ru.localizedName, 'Русский');
     expect(SupportedLocale.zh.localizedName, '简体中文');
     expect(SupportedLocale.de.localizedName, 'Deutsch');
